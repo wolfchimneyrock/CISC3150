465467560f66c49e27d52d3cb3ab80ce01a0670c
Robert Wagner <tholos@gmail.com>
2017-09-07 10:07:58 -0400

    hw2 q6

---
+++ hw2/Question6.java
@@ -0,0 +1,24 @@
+/********************************************************
+ * Robert Wagner
+ * CISC 3150 HW #2
+ * 2017-09-06
+ *
+ * Question6.java:
+ *   Delimit per character
+ *
+ ********************************************************/
+
+import java.util.*;
+
+class Question6 {
+    public static void main(String arg[]) {
+        Scanner s = new Scanner(System.in).useDelimiter("");
+        List<String> strs = new ArrayList<String>();
+        while (s.hasNext()) {
+            strs.add(s.next());
+        }
+        s.close();
+        for (String str : strs) 
+            System.out.println(str);
+    }
+}
